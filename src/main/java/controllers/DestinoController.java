71dfd4a38c8b119c415ad5dc7e0bb193ccd4fd31
DevTony101 <tonycortesano@gmail.com>
2020-06-30 17:53:27 -0500

    Creating the getData function

--- src/main/java/controllers/DestinoController.java
+++ src/main/java/controllers/DestinoController.java
@@ -1,11 +1,22 @@
 package controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import pojos.Usuario;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DestinoController implements Initializable {
+
+    @FXML
+    public AnchorPane raiz;
+
+    @FXML
+    private Label lblNombre, lblCiudad, lblApellido;
 
     /**
      * Called to initialize a controller after its root element has been
@@ -16,7 +27,15 @@ public class DestinoController implements Initializable {
      * @param resources The resources used to localize the root object, or <tt>null</tt> if
      */
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources) {}
 
+    @FXML
+    public void recuperarDatos() {
+        Stage stage = (Stage) this.raiz.getScene().getWindow(); // Paso 1
+        Usuario u = (Usuario) stage.getUserData(); // Paso 2
+        // Paso 3
+        lblNombre.setText(String.format("Nombre: %s", u.getNombre()));
+        lblApellido.setText(String.format("Apellido: %s", u.getApellido()));
+        lblCiudad.setText(String.format("Ciudad: %s", u.getCiudad()));
     }
 }
