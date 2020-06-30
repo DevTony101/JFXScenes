e6896b4cdf72676e14e052002205859497268d9f
DevTony101 <tonycortesano@gmail.com>
2020-06-30 18:37:54 -0500

    Adding the logic for the manual creation of the controller

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,11 +1,18 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import pojos.Usuario;
+import sun.security.krb5.internal.crypto.Des;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,6 +32,26 @@ public class HomeController implements Initializable {
         u.setNombre(nombre);
         u.setApellido(apellido);
         u.setCiudad(ciudad);
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+        try {
+            // Paso 1
+            FXMLLoader loader = new FXMLLoader(HomeController.class.getClassLoader().getResource("fxml/Destino.fxml"));
+            // Paso 2
+            DestinoController controller = new DestinoController();
+            controller.setUsuario(u);
+            // Paso 3
+            loader.setController(controller);
+            // Paso 4
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setTitle("Tutorial JavaFX");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println(String.format("Error creando ventana: %s", e.getMessage()));
+        }
     }
 
     /**
