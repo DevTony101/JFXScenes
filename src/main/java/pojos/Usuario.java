020c1ef0197ad442d30107da157d54fa202ff1fa
DevTony101 <tonycortesano@gmail.com>
2020-06-29 19:04:26 -0500

    Adding a simple POJO for a user using lombok

---
+++ src/main/java/pojos/Usuario.java
@@ -0,0 +1,14 @@
+package pojos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public final class Usuario {
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+}
