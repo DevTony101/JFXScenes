c016ad4a356c6b520c12b6ad417c0160b6c258d4
DevTony101 <tonycortesano@gmail.com>
2020-06-30 17:53:14 -0500

    Adding code for set user data in the stage

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,11 +1,18 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import pojos.Usuario;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,6 +32,19 @@ public class HomeController implements Initializable {
         u.setNombre(nombre);
         u.setApellido(apellido);
         u.setCiudad(ciudad);
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+        try {
+            Parent root = FXMLLoader.load(HomeController.class.getClassLoader().getResource("fxml/Destino.fxml"));
+            stage.setUserData(u);
+            Scene scene = new Scene(root);
+            stage.setTitle("Tutorial JavaFX");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println(String.format("Error creando ventana: %s", e.getMessage()));
+        }
     }
 
     /**
